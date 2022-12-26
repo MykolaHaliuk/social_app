10f9e228199d20f81f513edf757756205cf2a8fb
Mykola Haliuk <mykola.haliuk@plus4u.net>
2022-12-26 17:50:36 +0200

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,10 @@ import {
   Redirect,
 } from "react-router-dom";
 
-// import Home from "./pages/home/Home";
+import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 // import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
-
 // import Messenger from "./pages/messenger/Messenger";
 import { useAuthContext } from "./context/use-auth-context";
 import { observer } from "mobx-react-lite";
@@ -30,17 +29,16 @@ function App() {
       <CircularProgress />
     </Box>;
   }
-  console.log("-> authStore.isAuth132", authStore.isAuth );
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          {authStore.isAuth ? 123 : <Register />}
+          {authStore.isAuth ? <Home /> : <Register />}
         </Route>
         <Route path="/login">{authStore.isAuth ? <Redirect to="/" /> : <Login />}</Route>
-        {/*<Route path="/register">*/}
-        {/*  {authStore.isAuth ? <Redirect to="/" /> : <Register />}*/}
-        {/*</Route>*/}
+        <Route path="/register">
+          {authStore.isAuth ? <Redirect to="/" /> : <Register />}
+        </Route>
         {/*<Route path="/messenger">*/}
         {/*  {!authStore.isAuth ? <Redirect to="/" /> : <Messenger />}*/}
         {/*</Route>*/}
